e4feb2ead7209ee244f16d401058f99f61863b52
mariana3299 <mariana.ag3299@gmail.com>
2024-03-25 14:41:41 -0600

    feat: desktop contact layout finished

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,28 +1,28 @@
 export default function Form() {
   return (
     <>
-      <form action="" className="mx-8 flex flex-col gap-3">
+      <form action="" className="flex flex-col gap-3">
         <div className="flex flex-col">
-          <legend className="font-bold text-sm">Nombre</legend>
+          <legend className="font-bold text-sm lg:text-base">Nombre</legend>
           <input
             type="text"
             className="border-[0.5px] border-gray-600 rounded-lg p-2"
           />
         </div>
         <div className="flex flex-col">
-          <legend className="font-bold text-sm">Apellido</legend>
+          <legend className="font-bold text-sm lg:text-base">Apellido</legend>
           <input type="text" className="border border-gray-600 rounded-lg p-2" />
         </div>
         <div className="flex flex-col">
-          <legend className="font-bold text-sm">Correo</legend>
+          <legend className="font-bold text-sm lg:text-base">Correo</legend>
           <input type="email" className="border border-gray-600 rounded-lg p-2" />
         </div>
         <div className="flex flex-col">
-          <legend className="font-bold text-sm">Teléfono</legend>
+          <legend className="font-bold text-sm lg:text-base">Teléfono</legend>
           <input type="tel" className="border border-gray-600 rounded-lg p-2" />
         </div>
         <div className="flex flex-col">
-          <legend className="font-bold text-sm">
+          <legend className="font-bold text-sm lg:text-base">
             Preguntas y/o comentarios
           </legend>
           <textarea
@@ -30,11 +30,11 @@ export default function Form() {
             id=""
             cols="30"
             rows="5"
-            className="border border-gray-600 rounded-lg p-2" 
+            className="border border-gray-600 rounded-lg p-2 resize-none"  
           ></textarea>
         </div>
 
-        <button type="submit" className="bg-green-34 text-white font-semibold w-32 h-11 rounded-md text-base self-center shadow-xl mt-3">Enviar</button>
+        <button type="submit" className="bg-green-34 text-white font-semibold lg:text-xl w-32 h-11 lg:w-40 lg:h-14 rounded-md text-base self-center shadow-xl mt-3">Enviar</button>
       </form>
     </>
   );
