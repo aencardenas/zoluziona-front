272b9ff5918ec169cefd04181bd2416d60c6946e
Andres Cardenas <aencardenas@gmail.com>
2024-03-26 14:29:56 -0600

    feat: CharacteriticCard and Characteristic page component completed. Implemented CharacteristicCard toogle

--- src/components/CharacteristicCard.jsx
+++ src/components/CharacteristicCard.jsx
@@ -1,4 +1,4 @@
-import data from "../data/characteristics.json";
+import { useState } from "react";
 import ambient from "../assets/icons/ambiental.png";
 import fastCharge from "../assets/icons/carga-rapida.png";
 import lifeCycle from "../assets/icons/ciclo-vida.png";
@@ -8,12 +8,20 @@ import discharge from "../assets/icons/tasa-descarga.png";
 import temperature from "../assets/icons/temperatura.png";
 
 
-export default function CharacteristicCard() {
+export default function CharacteristicCard({ characteristic }) {
+
+    const [showList, setShowList] = useState(false);
+
+    const toggleList = () => {
+        setShowList(!showList)
+    }
+
+    const { name, descriptions, img } = characteristic
 
     const images = {
         "Alta Seguridad": security,
         "Ciclo de vida prolongado": lifeCycle,
-        "Capacidade de carga rápida": fastCharge,
+        "Capacidad de carga rápida": fastCharge,
         "Alta densidad de energía": energyDensity,
         "Baja tasa de autodescarga": discharge,
         "Amplio rango de temperatura": temperature,
@@ -25,7 +33,22 @@ export default function CharacteristicCard() {
     return (
 
         <>
-            
+            <div className="flex flex-col bg-white px-8 py-2 rounded-[10px]">
+                <div className="flex justify-start items-center">
+                    <svg className="w-10 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" onClick={toggleList}>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" />
+                    </svg>
+                    <img src={images[name]} className="w-16" alt={name}></img>
+                    <span className="font-bold text-xl w-full">{name}</span>
+                </div>
+                {showList && (
+                    <ul className="flex flex-col gap-y-4 list-disc text-justify text-">
+                        {descriptions.map((description, index) => (
+                            <li key={index}>{description}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </>
 
     );
--- src/pages/Characteristic.jsx
+++ src/pages/Characteristic.jsx
@@ -1,12 +1,21 @@
 import Header from "../utils/Header";
 import Footer from "../utils/Footer";
+import CharacteristicCard from "../components/CharacteristicCard";
+import data from "../data/characteristics.json";
 
 export default function Characteristic() {
     return (
 
         <>
-            <Header />
-            <Footer />
+            <div className="bg-green-28 min-h-screen flex flex-col">
+                <Header />
+                <div className="mt-4 mx-4 flex flex-col gap-8 mb-4">
+                    {data.characteristics.map((characteristic, index) => (
+                        <CharacteristicCard key={index} characteristic={characteristic}/>
+                    ))}
+                </div>
+                <Footer />
+            </div>
         </>
 
     );
