b71cc2ca04e402dce5f4ddcd560a1f1cee18738b
mariana3299 <mariana.ag3299@gmail.com>
2024-04-15 20:05:51 -0600

    feat: adding dvh to HomePowerGreen

--- src/pages/HomePowerGreen.jsx
+++ src/pages/HomePowerGreen.jsx
@@ -5,22 +5,20 @@ import { Link } from "react-router-dom";
 export default function HomePowerGreen() {
   return (
     <>
-      <div className="flex flex-col h-screen">
-        <Header />
-        <div className="lg:bg-banner-powerGreen bg-banner-powerGreen-mobile flex-1 bg-cover bg-center flex flex-col justify-center items-center">
-          <div className="pt-[23.5rem] flex flex-col lg:flex-row gap-8 justify-center items-center">
-            <p className="text-white text-4xl text-center">
-              Sitio en construcción
-            </p>
-            <Link to="/home">
-              <Button
-                text="Conoce POWER"
-                textColor="text-white"
-                bgHover="bg-blue-85"
-                bgActive="bg-blue-85"
-              />
-            </Link>
-          </div>
+      <Header />
+      <div className="lg:bg-banner-powerGreen bg-banner-powerGreen-mobile flex-1 bg-cover bg-center flex flex-col justify-center items-center h-dvh">
+        <div className="pt-[17rem] flex flex-col lg:flex-row gap-8 justify-center items-center">
+          <p className="text-white text-4xl text-center">
+            Sitio en construcción
+          </p>
+          <Link to="/home">
+            <Button
+              text="Conoce POWER"
+              textColor="text-white"
+              bgHover="bg-blue-85"
+              bgActive="bg-blue-85"
+            />
+          </Link>
         </div>
       </div>
     </>
