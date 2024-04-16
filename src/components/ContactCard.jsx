d0be9ce8c4118ad0a2ff63f4b431ada51fc89747
mariana3299 <mariana.ag3299@gmail.com>
2024-04-15 18:52:22 -0600

    feat: contact cards updated

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,17 +1,17 @@
 import logo from "../assets/images/card-logo.png";
 import linea from "../assets/images/contacto-linea.png";
 
-export default function Card() {
+export default function Card({text, title}) {
   return (
     <>
-      <div className="flex justify-center items-center py-11">
-        <div className="flex justify-center items-center gap-2 shadow-md rounded-md h-[5.5rem] w-60 lg:w-[300px] lg:h-24">
+      <div className="flex justify-center items-center w-full">
+        <div className="flex justify-center items-center gap-2 shadow-md rounded-md h-[5.5rem]  lg:h-24 w-full">
           <img src={logo} alt="logo" className="w-14 h-14 lg:h-16 lg:w-16" />
           <img src={linea} alt="" className="h-16 lg:h-[70px]" />
           <div className="text-sm text-blue-70 lg:text-base">
-            <p className="font-bold">Ventas</p>
+            <p className="font-bold">{title}</p>
             <p>Mérdia, Yucatán</p>
-            <p>correo@vetnas.com</p>
+            <p className="text-sm">{text}</p>
           </div>
         </div>
       </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,8 +11,10 @@ export default function Contact() {
         <h2 className="my-3 font-bold text-xl lg:text-3xl">Contáctanos</h2>
 
         <Form />
-
-        <Card />
+        <div className="py-11 flex flex-col gap-4 justify-center items-center lg:flex-row lg:justify-around">
+          <Card text="ventas@zoluzionapower.com" title='Ventas' />
+          <Card text="contacto@zoluzionapower.com" title='Contacto' />
+        </div>
       </div>
 
       <Footer />
