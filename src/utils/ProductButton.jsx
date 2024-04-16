e8a7158ad4352e25ed5e15cf0e482631bba9aefd
mariana3299 <mariana.ag3299@gmail.com>
2024-04-15 20:02:05 -0600

    feat: adding _target:blank to download button

--- src/utils/ProductButton.jsx
+++ src/utils/ProductButton.jsx
@@ -5,6 +5,7 @@ export default function ProductButton({text, onClick, downloadURL}){
                 href={downloadURL}
                 download
                 className="bg-blue-62 text-white font-semibold rounded-md py-3 px-5 text-lg hover:bg-blue-85 w-full text-left"
+                target="_blank"
             >
                 {text}
             </a>
